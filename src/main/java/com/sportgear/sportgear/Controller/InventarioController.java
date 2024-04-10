1dc6fe3d8f76ddff42154935de5aa3f8c40d7f3d
Manuel <manejo03@outlook.es>
2024-04-09 19:08:16 -0500

    Update InventarioController

--- src/main/java/com/sportgear/sportgear/Controller/InventarioController.java
+++ src/main/java/com/sportgear/sportgear/Controller/InventarioController.java
@@ -11,8 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
