3680ae53d5c7337bbd2bb23d7e638571d1e73bfe
Manuel <manejo03@outlook.es>
2024-04-21 18:27:32 -0500

    Add logica Devolucion

---
+++ src/main/java/com/sportgear/sportgear/Service/DevolucionesService.java
@@ -0,0 +1,8 @@
+package com.sportgear.sportgear.Service;
+
+import com.sportgear.sportgear.Model.Devoluciones;
+
+public interface DevolucionesService {
+
+    public void solicitudDevolucion(Devoluciones devoluciones);
+}
