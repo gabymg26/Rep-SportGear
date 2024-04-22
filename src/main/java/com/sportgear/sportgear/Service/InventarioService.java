acd7e9ad4f44743fdf40366eda77765b62274496
gabymg26 <gabrielamoralesgarcia2004@gmail.com>
2024-04-22 11:46:53 -0500

    Add paginación

--- src/main/java/com/sportgear/sportgear/Service/InventarioService.java
+++ src/main/java/com/sportgear/sportgear/Service/InventarioService.java
@@ -1,7 +1,8 @@
 package com.sportgear.sportgear.Service;
 
 import com.sportgear.sportgear.Model.Inventario;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface InventarioService {
@@ -10,4 +11,5 @@ public interface InventarioService {
     public void guardar(Inventario inventario);
     public Inventario buscarPorId(Long id);
     public void eliminar(Long id);
+    public Page<Inventario> listarInventarioPaginado(Pageable pageable);
 }
